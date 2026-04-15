6220642f35d61027b3a9b1f415320b88b3d65e96
jioooo20 <giojio825@gmail.com>
2026-04-15 16:40:12 +0700

    DAY 1 Enhance form validation and add delete functionality: require input fields in index.html and implement delete feature in script.js

--- fe/script.js
+++ fe/script.js
@@ -33,6 +33,16 @@ async function tambahData(judul, isi) {
     return await response.json();
 }
 
+async function hapusData(id) {
+    const response = await fetch('http://localhost:3000/catatan/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id })
+    })
+}
+
 function tampilkanData(catatan) {
     const container = document.querySelector('.daftar-catatan')
     container.innerHTML = '';
@@ -50,6 +60,7 @@ function tampilkanData(catatan) {
             <tr>
                 <th>Judul</th>
                 <th>Isi</th>
+                <th>Aksi</th>
             </tr>
         </thead>
         <tbody>
@@ -60,11 +71,27 @@ function tampilkanData(catatan) {
 
     catatan.forEach(c => {
         const tr = document.createElement('tr');
+
+        const btnDelete = document.createElement('button');
+        btnDelete.className = 'btn btn-danger';
+        btnDelete.textContent = 'Hapus';
+        btnDelete.onclick = async () => {
+            if (confirm('Yakin hapus data?')) {
+                await hapusData(c.id);
+                const dataTerbaru = await ambilData();
+                tampilkanData(dataTerbaru);
+            }
+        }
+
         tr.innerHTML = `
         <td>${c.judul}</td>
         <td>${c.isi}</td>
+        <td></td>
         `;
+
+        tr.cells[2].appendChild(btnDelete);
         tbody.appendChild(tr);
+
     });
     container.appendChild(table);
 }
@@ -78,7 +105,7 @@ document.getElementById('form-catatan').addEventListener('submit', async (e) => 
     try {
         await tambahData(judul, isi);
         e.target.reset();
-        
+
         const dataTerbaru = await ambilData();
         tampilkanData(dataTerbaru);
     } catch (error) {
@@ -86,7 +113,7 @@ document.getElementById('form-catatan').addEventListener('submit', async (e) => 
     }
 });
 
-document.addEventListener('DOMContentLoaded', async()=>{
+document.addEventListener('DOMContentLoaded', async () => {
     try {
         const data = await ambilData();
         tampilkanData(data);
